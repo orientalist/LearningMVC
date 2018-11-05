757942b9e28bc81aa4d61e75c53189bf566f149d
x774436248 <x774436248@gmail.com>
2018-11-05 09:11:28 +0800

    initialize files

---
+++ Controllers/Learning.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+namespace LearningMVC.Controllers{
+    public class LearningController:Controller{
+        public string Index(){
+
+            return "View()";
+        }
+    }
+}
